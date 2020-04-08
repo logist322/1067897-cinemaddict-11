1a9329edb2965ba09e3d6109002624c3f3048ee6
silentPartner <hytornoy-99@mail.ru>
2020-04-08 22:16:54 +0300

    Done

---
+++ src/components/most-commented.js
@@ -0,0 +1,10 @@
+export const createMostCommentedTemplate = () => {
+  return (
+    `<section class="films-list--extra films-list--most">
+      <h2 class="films-list__title">Most commented</h2>
+
+      <div class="films-list__container">
+      </div>
+    </section>`
+  );
+};
---
+++ src/components/top-rate.js
@@ -0,0 +1,10 @@
+export const createTopRateTemplate = () => {
+  return (
+    `<section class="films-list--extra films-list--top">
+      <h2 class="films-list__title">Top rated</h2>
+
+      <div class="films-list__container">
+      </div>
+    </section>`
+  );
+};
